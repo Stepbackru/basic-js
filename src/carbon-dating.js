cd465fb23ee92299b43582f8a7800a76ca0b28dc
Stepbackru <alex-ruba@bk.ru>
2020-02-16 02:17:29 +0300

    feat: decided

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,17 +2,12 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  let num = Number(sampleActivity);
+  let num = parseFloat(sampleActivity);
+  const K = 0.693 / HALF_LIFE_PERIOD;
   if(Number.isNaN(num) || (typeof sampleActivity !== 'string') || (num <= 0 || num > 15) ){
     return false
   } else {
-    let k = 0.693 / HALF_LIFE_PERIOD;
-    let t = Math.ceil((Math.log(MODERN_ACTIVITY / num)) / k);
+    let t = Math.ceil((Math.log(MODERN_ACTIVITY / num)) / K);
     return t;
   }
 };
-
-//для теста в консоли браузера
-// let n = Number(x);
-// let k = 0.693 / 5730;
-// let t = Math.ceil((Math.log(15 / n)) / k);
