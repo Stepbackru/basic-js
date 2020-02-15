3335b9ae3ce5daa4e5eb86aa3c8b83515172a72d
Stepbackru <alex-ruba@bk.ru>
2020-02-16 00:12:47 +0300

    feat: decided half

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,19 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let num = Number(sampleActivity);
+  if(Number.isNaN(num) || (typeof sampleActivity !== 'string') || (num <= 0)){
+    return false
+  } else {
+    let n = Number(sampleActivity);
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let t = Math.ceil((Math.log(MODERN_ACTIVITY / n)) / k);
+    return t;
+  }
 };
+
+//для теста в консоли браузера
+// let n = Number(x);
+// let k = 0.693 / 5730;
+// let t = Math.ceil((Math.log(15 / n)) / k);
