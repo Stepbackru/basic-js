b51f1cb9a628bca0564f1d47f7afdd14bc227a45
Stepbackru <alex-ruba@bk.ru>
2020-02-20 00:02:41 +0300

    feat: decided

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,21 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let newArr = [];
+    if(Array.isArray(arr)){
+        for(let i = 0; i < arr.length; i++) {
+            if(arr[i] === '--discard-next'){
+                i++;
+            } else if(arr[i] === '--discard-prev'){
+                newArr.pop();
+            } else if(arr[i] === '--double-next'){
+                newArr.push(arr[i + 1]);
+            } else if(arr[i] === '--double-prev'){
+                newArr.push(arr[i - 1]);
+            } else {
+                newArr.push(arr[i]);
+            }
+        };
+    } else {
+        throw new Error();
+    }
+    return newArr.filter(e => e !== undefined);
 };
